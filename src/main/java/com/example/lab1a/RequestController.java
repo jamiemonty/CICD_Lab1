76f482c2bfbf2afffeb6d0490cf39dcf1784db9a
G00419525@atu.ie <G00419525@atu.ie>
2024-09-25 15:18:13 +0100

    Created a new java class, added  code to print a message to the localhost url.

---
+++ src/main/java/com/example/lab1a/RequestController.java
@@ -0,0 +1,18 @@
+package com.example.lab1a;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class RequestController {
+    @GetMapping("/hello")
+    public String message()
+    {
+        return "Hello";
+    }
+    @GetMapping("/greet/{name}")
+    public String greetByName(@PathVariable String name) {
+        return "Hello there " + name;
+    }
+}
