f82eba16b0797c1b784f942ee0715c1d85f4cd52
G00419525@atu.ie <G00419525@atu.ie>
2024-09-25 15:14:37 +0100

    Created a new project for my first CICD lab.

---
+++ src/main/java/com/example/lab1a/Lab1aApplication.java
@@ -0,0 +1,13 @@
+package com.example.lab1a;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab1aApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lab1aApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/lab1a/Lab1aApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lab1a;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab1aApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
