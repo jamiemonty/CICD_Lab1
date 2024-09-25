1ff8e213c16f6b800a83c096c9d6e0741a729892
G00419525@atu.ie <G00419525@atu.ie>
2024-09-25 15:26:34 +0100

    Added a method so the website prints your name and age out after you enter it into the search bar

--- src/main/java/com/example/lab1a/RequestController.java
+++ src/main/java/com/example/lab1a/RequestController.java
@@ -2,6 +2,7 @@ package com.example.lab1a;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -14,5 +15,10 @@ public class RequestController {
     @GetMapping("/greet/{name}")
     public String greetByName(@PathVariable String name) {
         return "Hello there " + name;
+    }
+    @GetMapping("/details")
+    public String details(@RequestParam String name, @RequestParam int age)
+    {
+        return name + " " + age;
     }
 }
